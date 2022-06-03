b023d24b1700ae645e5b525d64faffdcb0459781
Aycan45 <aycanikom@abv.bg>
2022-06-03 10:19:05 +0300

    working login and added username to register

--- classes/register.class.php
+++ classes/register.class.php
@@ -4,8 +4,8 @@
 
         protected function setUser($username, $email, $password)
         {
-            $statement = $this->connect()->prepare("INSERT INTO users (`USERNAME` `EMAIL`, `PASSWORD`) VALUES (:username,:email,:password);");   
-
+            $statement = $this->connect()->prepare("INSERT INTO users (`USERNAME`, `EMAIL`, `PASSWORD`) VALUES (:username,:email,:password);");  
+            
             $hashpassword = password_hash($password, PASSWORD_DEFAULT);
 
             $statement->bindValue('username', $username, PDO::PARAM_STR);
@@ -22,7 +22,7 @@
                 }
             } catch (Exception $e) {
                 echo $e->getMessage();
-                header("location: login.html");
+                exit();
             }
 
             $statement=null;
