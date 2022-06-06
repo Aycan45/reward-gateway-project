6fae97d2dfba569d336c236b9def9166dc72c7a9
Aycan45 <aycanikom@abv.bg>
2022-06-06 13:18:48 +0300

    removed some parts of the index html code

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
 <?php 
     session_start();
+
+    include('./includes/header.inc.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <title>eCard sending system</title>
-</head>
 <body>
-    <?php
-        include('./includes/header.inc.php');
-    ?>
+    
     <div class="row m-2">
     <?php 
         include("./classes/databasehelper.class.php");
